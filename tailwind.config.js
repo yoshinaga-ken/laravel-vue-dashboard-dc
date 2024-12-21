f3f29239ed7ca99ad4ba403fbad7c11586a91f30
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2024-12-21 17:39:32 +0900

    Install Jetstream

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
+
     theme: {
         extend: {
             fontFamily: {
@@ -16,5 +19,6 @@ export default {
             },
         },
     },
-    plugins: [],
+
+    plugins: [forms, typography],
 };
