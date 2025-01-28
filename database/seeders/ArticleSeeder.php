8ed60f4c05d15f99d1a020f2c42e4a9b3962ebf0
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2025-01-28 15:14:47 +0900

    feat(seeder): add like seeder & update other seeders
    - usage: $php artisan db:seed --class=LikeSeeder

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ArticleSeeder extends Seeder
@@ -11,7 +11,15 @@ class ArticleSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void {
-        Article::factory(10)->create();
+    public function run(): void
+    {
+        // ダミーの記事をN件作る
+        Article::factory(20)->create();
+
+        // TEST_USER_NAMEの記事をN件作る
+        $user = User::where('name', env('TEST_USER_NAME'))->first();
+        Article::factory(20)->createQuietly([
+            'user_id' => $user->id,
+        ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->withPersonalTeam()->create([
+            'name' => env('TEST_USER_NAME', 'Test User'),
+            'email' => env('TEST_USER_EMAIL', 'test@example.com'),
+        ]);
+        User::factory(10)->withPersonalTeam()->create();
+    }
+}
