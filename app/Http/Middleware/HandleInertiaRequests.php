245241b34bb035ebaa55ae74cad89dec2e803553
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2025-02-08 19:03:37 +0900

    feat: ユーザープロファイル画面 - フォロワー情報表示するように修正

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,6 +35,10 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        if ($request->route()->getName() == 'profile.show') {
+            $request->user()->load('followers', 'following');
+        }
+
         return array_merge(parent::share($request), [
             'flash' => function () use ($request) {
                 return [
--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -6,6 +6,8 @@ import SectionBorder from '@/Components/SectionBorder.vue';
 import TwoFactorAuthenticationForm from '@/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue';
 import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue';
+import UpdateProfileArticlesForm from "@/Pages/Profile/Partials/UpdateProfileArticlesForm.vue";
+import ActionSection from "@/Components/ActionSection.vue";
 
 defineProps({
     confirmsTwoFactorAuthentication: Boolean,
@@ -27,6 +29,40 @@ defineProps({
                     <UpdateProfileInformationForm :user="$page.props.auth.user" />
 
                     <SectionBorder />
+                </div>
+
+                <div>
+                  <ActionSection class="mt-10 sm:mt-0">
+                    <template #title>
+                      Followers Information
+                    </template>
+
+                    <template #description>
+
+                    </template>
+
+                    <template #content>
+                      <div class="grid grid-cols-2">
+                        <div>
+                          <div>
+                            <span class="font-bold">{{ $page.props.auth.user.followers.length }}</span> followers
+                          </div>
+                          <ul v-for="follower in $page.props.auth.user.followers" :key="follower.id">
+                            <li>{{ follower.name }}</li>
+                          </ul>
+                        </div>
+                        <div>
+                          <div>
+                            <span class="font-bold">{{ $page.props.auth.user.following.length }}</span> following
+                          </div>
+                          <ul v-for="following in $page.props.auth.user.following" :key="following.id">
+                            <li>{{ following.name }}</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </template>
+                  </ActionSection>
+                  <SectionBorder/>
                 </div>
 
                 <div v-if="$page.props.jetstream.canUpdatePassword">
