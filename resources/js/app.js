dda04bbb9b6455f2684681d6e5db4d86469ce841
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2025-02-11 15:45:07 +0900

    feat(style): vuetify & mdi/font mdiがglobalで使用化へ

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,37 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy';
+
+// element-plus
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'element-plus/dist/index.css'
 import 'animate.css'
 
-import { updateDarkModeClass } from './Utils/utils.js';
+// vuetify
+// import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+import {updateDarkModeClass} from './Utils/utils.js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
+    theme: {
+        defaultTheme: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+    },
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -22,6 +42,7 @@ createInertiaApp({
         app
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
 
         updateDarkModeClass();
