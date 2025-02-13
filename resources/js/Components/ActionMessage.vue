21d3942fd7f8dd514b696a8bc48fea61bc14330f
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2025-02-13 18:00:08 +0900

    feat(component): ActionMessage fadeIn/fadeOut オプション追加

--- resources/js/Components/ActionMessage.vue
+++ resources/js/Components/ActionMessage.vue
@@ -1,15 +1,30 @@
 <script setup>
 defineProps({
-    on: Boolean,
+  on: Boolean,
+  fadeIn: {
+    type: Boolean,
+    default: false,
+  },
+  fadeOut: {
+    type: Boolean,
+    default: true,
+  },
 });
 </script>
 
 <template>
-    <div>
-        <transition leave-active-class="transition ease-in duration-1000" leave-from-class="opacity-100" leave-to-class="opacity-0">
-            <div v-show="on" class="text-sm text-gray-600 dark:text-gray-400">
-                <slot />
-            </div>
-        </transition>
-    </div>
+  <div>
+    <transition
+      :enter-active-class="fadeIn ? 'transition ease-out duration-1000' : ''"
+      :enter-from-class="fadeIn ? 'opacity-0' : ''"
+      :enter-to-class="fadeIn ? 'opacity-100' : ''"
+      :leave-active-class="fadeOut ? 'transition ease-out duration-1000' : ''"
+      :leave-from-class="fadeOut ? 'opacity-100' : ''"
+      :leave-to-class="fadeOut ? 'opacity-0' : ''"
+    >
+      <div v-show="on" class="text-sm text-gray-600 dark:text-gray-400">
+        <slot/>
+      </div>
+    </transition>
+  </div>
 </template>
