0d3e120c903c98e79cb8226049e9c401b9d7e4dc
Kenichi Yoshinaga <k_yoshinaga@annai.co.jp>
2025-02-18 13:45:48 +0900

    feat(service): Implement MetaService for dynamic metadata handling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        @php
+            $meta = app(App\Services\MetaService::class)->getMetaData(request()->query('data'));
+        @endphp
+        <title inertia>{{ $meta['title'] }}</title>
+        <link rel="shortcut icon" href="{{ $meta['favicon'] }}">
+        <meta name="keywords" content="{{ $meta['keywords'] }}">
+        <meta name="description" content="{{ $meta['description'] }}">
+        <meta property="og:image" content="{{ $meta['og:image'] }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
