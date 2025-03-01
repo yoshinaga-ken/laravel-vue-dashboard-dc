4088963b3fba6bd721016e1f1787640e95121888
Kenichi Yoshinaga <yottushy75@gmail.com>
2025-03-01 19:39:41 +0900

    feat(article): enhance ArticleLikeButton with slot support and attribute binding

--- resources/js/Components/ArticleLikeButton.vue
+++ resources/js/Components/ArticleLikeButton.vue
@@ -19,12 +19,14 @@ const likeUserNames = computed(() => {
           :title="likeUserNames"
           class="rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
           :class="[ article.is_liked_by ? 'bg-red-200' : 'bg-gray-200', disabled ? 'opacity-50' : 'opacity-100' ]"
-          :disabled="disabled">
+          :disabled="disabled"
+          v-bind="$attrs">
     <v-icon icon="mdi-thumb-up"
             class="text-pink-500 mr-1"
             :class="{'animate__heartBeat':article.is_liked_by}"
     />
     {{ article.likes?.length }}
+    <slot/>
   </button>
 
   <template v-if="isUserList">
