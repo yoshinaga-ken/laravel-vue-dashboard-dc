6f1de6a3672446053f5f71aaa72ce77c9e9fcdd0
Kenichi Yoshinaga <yottushy75@gmail.com>
2025-04-08 19:03:33 +0900

    feat(graphql): add TypeScript types and codegen configuration for GraphQL
    - $ pnpm graphql-codegen init
    - $ pnpm codegen

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://127.0.0.1:8000/graphql", // GraphQLエンドポイントを指定
+  documents: "graphql/**/*.graphql", // GraphQLクエリファイルのパスを指定
+  generates: {
+    "resources/js/Types/types-graphql.d.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-resolvers"
+      ]
+    }
+  }
+};
+
+export default config;
