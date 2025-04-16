783a823771bbcf5f438a13051448086675f87431
Kenichi Yoshinaga <yottushy75@gmail.com>
2025-04-16 14:51:01 +0900

    feat(components): [VfTextTagsInput] タグ入力コンポーネントの新規作成 with Vuetify Autocomplete
    - [vuetify - Autocomplete](https://vuetifyjs.com/en/components/autocompletes/)

---
+++ resources/js/Components/VfTextTagsInput.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import type { FilterTagInput, TagPaginator } from '@/Types/types-graphql'
+
+defineProps({
+  disabled: Boolean,
+  inputPlaceholder: {
+    type: String,
+    default: 'Search Tag'
+  },
+  clearable: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const selectedTags = defineModel<string[]>({
+  default: () => []
+});
+
+const inputValue = ref('');
+
+const { result } = useQuery<{ tags: TagPaginator }>(gql`
+  query FilterTags($input: FilterTagInput) {
+    tags(input: $input, first: 512) {
+      data {
+        name
+      }
+    }
+  }
+`, {
+  variables: {
+    input: {
+      name: ''
+    } satisfies FilterTagInput
+  }
+})
+
+const availableTags = computed(() => {
+  if (!result.value?.tags?.data) return []
+  return result.value.tags.data.map((tag) => tag.name)
+})
+
+const handleInputConfirm = () => {
+  const currentInput = inputValue.value;
+  if (currentInput && !selectedTags.value.includes(currentInput)) {
+    selectedTags.value.push(currentInput);
+  }
+  inputValue.value = '';
+};
+</script>
+
+<template>
+  <v-autocomplete
+    v-model="selectedTags"
+    v-model:search="inputValue"
+    :items="availableTags"
+    :placeholder="inputPlaceholder"
+    :disabled="disabled"
+    :clearable="clearable"
+    chips
+    multiple
+    closable-chips
+    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300"
+    variant="outlined"
+    @keyup.enter="handleInputConfirm"
+  />
+</template>
+
+<style>
+.v-autocomplete #tags-messages {
+  display: none
+}
+</style>
