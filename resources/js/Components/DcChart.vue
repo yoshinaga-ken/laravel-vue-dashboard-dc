72bcbfec99b938a7caf665f9b755eea3b7410127
Kenichi Yoshinaga <yottushy75@gmail.com>
2025-05-13 21:23:14 +0900

    feat(DcChart): add new weather data CSV mappings for precipitation and temperature

--- resources/js/Components/DcChart.vue
+++ resources/js/Components/DcChart.vue
@@ -1134,6 +1134,8 @@ const props = defineProps({
     default: () => ({
       'covid19-japan.jpg': 'covid19-data-2021-02-28.csv',
       'food-ramen.jpg': 'food-ramen.csv',
+      'ja-weather-precipitation.jpg': 'ja-weather-precipitation.csv',
+      'ja-weather-temperature.jpg': 'ja-weather-temperature.csv',
       'ja-quake-noto-safety.jpg': 'ja-quake-noto-safety.csv',
       'ja-tokyo-gubernatorial-election.jpg': 'ja-tokyo-gubernatorial-election.csv',
       'resas-agriculture.jpg': 'resas-agriculture.csv',
