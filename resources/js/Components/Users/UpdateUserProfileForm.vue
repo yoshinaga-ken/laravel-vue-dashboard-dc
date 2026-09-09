c8960f7fa8cbc869d23ac0f4b99868d6887f5990
Kenichi Yoshinaga <yottushy75@gmail.com>
2026-09-09 20:15:57 +0900

    chore: upgrade Vite 8, Vitest 5, and ESLint 10

--- resources/js/Components/Users/UpdateUserProfileForm.vue
+++ resources/js/Components/Users/UpdateUserProfileForm.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
   import { ref, computed, watch } from 'vue'
-  import { ElForm, ElFormItem, ElInput, ElButton, ElAvatar, ElMessage, ElMessageBox } from 'element-plus'
+  import { ElForm, ElFormItem, ElInput, ElButton, ElAvatar, ElMessage } from 'element-plus'
   import { useUpdateUser, type UpdateUserInput } from '@/Composables/useUpdateUser'
   import { useUserProfile } from '@/Composables/useUserProfile'
   import type { User } from '@/Types/types-graphql'
