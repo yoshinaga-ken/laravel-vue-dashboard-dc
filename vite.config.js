fe51d9833b7031ea15be216a48a7d8dd24c85885
Kenichi Yoshinaga <yottushy75@gmail.com>
2026-09-09 20:08:49 +0900

    chore: upgrade Vuetify from 3 to 4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     optimizeDeps: {
-        include: ['wanakana'],
+        include: [
+            'wanakana',
+            // Vuetify 4: keep overlay stacking on a single useStack instance in Vite dev.
+            // https://vuetifyjs.com/en/getting-started/upgrade-guide/#vite-overlay-z-index-in-dev-mode
+            'vuetify/components/VOverlay',
+            'vuetify/components/VDialog',
+            'vuetify/components/VMenu',
+            'vuetify/components/VSelect',
+            'vuetify/components/VTooltip',
+        ],
     },
     plugins: [
         laravel({
